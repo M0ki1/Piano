660d36cccebbc5a9a872e842a9e420f8ad0d1da0
Matias Muñoz <mati.munoz.314@gmail.com>
2021-12-07 17:14:30 -0300

    Nuevas librerias jaj

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
-// document.addEventListener('keydown', (event) => {
-//     const keyName = event.key;
-//     alert('keydown event\n\n' + 'key: ' + keyName);
-//   });
+"use strict";
+// const performance = require('perf_hooks');
+const notes = {"z":"audio/Piano.mf.C5.mp3", "x":"audio/Piano.mf.D5.mp3",
+               "c":"audio/Piano.mf.E5.mp3", "v":"audio/Piano.mf.F5.mp3",
+               "b":"audio/Piano.mf.G5.mp3", "n":"audio/Piano.mf.A5.mp3",
+               "m":"audio/Piano.mf.B5.mp3", ",":"audio/Piano.mf.C6.mp3"};
+// var startTime = performance.now();
+document.addEventListener("keydown", (event) => {
+  const keyName = event.key;
+  var audio = new Audio(notes[keyName]);
+  audio.play();
+});
+// var endTime = performance.now();
+// console.log(`Call to doSomething took ${endTime - startTime} milliseconds`);
+//use howler module and test it
