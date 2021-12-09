af2e6e07b3d061d1503db5210a3d278a82ef9417
Matias Muñoz <mati.munoz.314@gmail.com>
2021-12-08 21:19:18 -0300

    añadimos dependencias pero no funciona jaj

---
+++ audio_test.js
@@ -0,0 +1,7 @@
+
+const {Howl, Howler} = require('howler');
+const sound = new Howl({
+    src: ['audio/Piano.mf.A5.mp3']
+  });
+  
+  sound.play();
--- test.js
+++ test.js
@@ -1,13 +1,16 @@
-"use strict";
-// const performance = require('perf_hooks');
-const notes = {"z":"audio/Piano.mf.C5.mp3", "x":"audio/Piano.mf.D5.mp3",
-               "c":"audio/Piano.mf.E5.mp3", "v":"audio/Piano.mf.F5.mp3",
-               "b":"audio/Piano.mf.G5.mp3", "n":"audio/Piano.mf.A5.mp3",
-               "m":"audio/Piano.mf.B5.mp3", ",":"audio/Piano.mf.C6.mp3"};
+'use strict';
+const {Howl, Howler} = require('howler');
+const notes = {'z':'audio/Piano.mf.C5.mp3', 'x':'audio/Piano.mf.D5.mp3',
+               'c':'audio/Piano.mf.E5.mp3', 'v':'audio/Piano.mf.F5.mp3',
+               'b':'audio/Piano.mf.G5.mp3', 'n':'audio/Piano.mf.A5.mp3',
+               'm':'audio/Piano.mf.B5.mp3', ',':'audio/Piano.mf.C6.mp3'};
 // var startTime = performance.now();
-document.addEventListener("keydown", (event) => {
+document.addEventListener('keydown', (event) => {
   const keyName = event.key;
-  var audio = new Audio(notes[keyName]);
+  const note = notes[keyName];
+  const audio = new Howl({
+    src:[note]
+  });
   audio.play();
 });
 // var endTime = performance.now();
